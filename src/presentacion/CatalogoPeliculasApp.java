54d330454dfdd1f1c7b40ea81a09c8b76f161de5
HIMT <himt823@gmail.com>
2023-10-09 16:18:20 -0300

    Se añade modulo Presentación y clase CatalogoPeliculas

---
+++ src/presentacion/CatalogoPeliculasApp.java
@@ -0,0 +1,64 @@
+package presentacion;
+
+import dominio.Pelicula;
+import servicio.IServicioPeliculas;
+import servicio.ServicioPeliculasLista;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class CatalogoPeliculasApp {
+    public static void main(String[] args) {
+       var salir = false;
+       var consola = new Scanner(System.in);
+       // Agregamos la implementación del servicio
+        IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+        while(!salir){
+            try{
+                mostrarMenu();
+                salir = ejecutarOpciones(consola, servicioPeliculas);
+            } catch (Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
+            System.out.println();
+        }// Fin while
+    }
+
+    private static void mostrarMenu(){
+        System.out.print("""
+                *** Catalogo de Peliculas ***
+                1. Agregar pelicula
+                2. Listar peliculas
+                3. Buscar pelicula
+                4. Salir
+                Elige una opción: 
+                """);
+    }
+
+    private static Boolean ejecutarOpciones(Scanner consola,
+                                            IServicioPeliculas servicioPeliculas){
+
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        switch (opcion){
+            case 1 -> {
+                System.out.println("Introduce el nombre de la pelicula: ");
+                var nombrePelicula = consola.nextLine();
+                servicioPeliculas.agregarPelicula(new Pelicula(nombrePelicula));
+            }
+            case 2 -> servicioPeliculas.listarPeliculas();
+            case 3 -> {
+                System.out.println("Introduce la pelicula a buscar: ");
+                var buscar = consola.nextLine();
+                servicioPeliculas.buscarPelicula(new Pelicula(buscar));
+            }
+            case 4 -> {
+                System.out.println("Hasta pronto!");
+                salir = true;
+            }
+            default -> System.out.println("Opción no reconocida: " + opcion);
+        }
+        return salir;
+    }
+}
