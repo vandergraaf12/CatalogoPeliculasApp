8311d8e43a481906c8123e7e0d73b6b8871cba76
HIMT <himt823@gmail.com>
2023-10-09 15:58:49 -0300

    Prueba servicio IServicioPeliculas y control existencia pelicula buscada

--- src/servicio/ServicioPeliculasLista.java
+++ src/servicio/ServicioPeliculasLista.java
@@ -21,14 +21,31 @@ public class ServicioPeliculasLista implements IServicioPeliculas{
 
     @Override
     public void agregarPelicula(Pelicula pelicula) {
-        peliculas.add(pelicula):
-        System.out.println("Se agregó la peliculas: " + pelicula);
+        peliculas.add(pelicula);
+        System.out.println("Se agregó la pelicula: " + pelicula);
     }
 
     @Override
     public void buscarPelicula(Pelicula pelicula) {
         // Regresa el indice de la pelicula encontrada en la lista
         var indice = peliculas.indexOf(pelicula);
-        System.out.println("Pelicula encontrada en el indice: " + indice);
+        if(indice == -1) System.out.println("No se encontró la pelicula buscada: " + pelicula);
+        else System.out.println("Pelicula encontrada en el indice: " + indice);
+    }
+
+    public static void main(String[] args) {
+        // Creamos algunos objetos de tipo pelicula
+        var pelicula1 = new Pelicula("Batman");
+        var pelicula2 = new Pelicula("Superman");
+        // Creamos el servicio (patron de disenio service)
+        IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+        // Agregamos las peliculas a la lista
+        servicioPeliculas.agregarPelicula(pelicula1);
+        servicioPeliculas.agregarPelicula(pelicula2);
+        // Listamos las peliculas
+        servicioPeliculas.listarPeliculas();
+        // Buscamos una pelicula
+        // Se debe implementar el metodo Equals y Hashcode
+        servicioPeliculas.buscarPelicula(pelicula1);
     }
 }
