c321f21d135e07d64b518f6750d0b11df6852c97
HIMT <himt823@gmail.com>
2023-10-09 15:48:47 -0300

    Se añade paquete servicios, interfaz IServicioPeliculas e implementación ServicioPeliculasList

---
+++ src/servicio/IServicioPeliculas.java
@@ -0,0 +1,9 @@
+package servicio;
+
+import dominio.Pelicula;
+
+public interface IServicioPeliculas {
+    public void listarPeliculas();
+    public void agregarPelicula(Pelicula pelicula);
+    public void buscarPelicula(Pelicula pelicula);
+}
---
+++ src/servicio/ServicioPeliculasLista.java
@@ -0,0 +1,34 @@
+package servicio;
+
+import dominio.Pelicula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioPeliculasLista implements IServicioPeliculas{
+
+    private final List<Pelicula> peliculas;
+
+    public ServicioPeliculasLista(){
+        this.peliculas = new ArrayList<>();
+    }
+
+    @Override
+    public void listarPeliculas() {
+        System.out.println("Listado de peliculas...");
+        peliculas.forEach(System.out::println);
+    }
+
+    @Override
+    public void agregarPelicula(Pelicula pelicula) {
+        peliculas.add(pelicula):
+        System.out.println("Se agregó la peliculas: " + pelicula);
+    }
+
+    @Override
+    public void buscarPelicula(Pelicula pelicula) {
+        // Regresa el indice de la pelicula encontrada en la lista
+        var indice = peliculas.indexOf(pelicula);
+        System.out.println("Pelicula encontrada en el indice: " + indice);
+    }
+}
